226ea656602b01306f042b4fe33ecb0efa7791f3
Paulus-AllodsLegacy <paulus.allods.nova@gmail.com>
2019-04-25 23:03:18 -0700

    Remove TODO

--- Database/Resource/Implementation/ModelPart.cs
+++ Database/Resource/Implementation/ModelPart.cs
@@ -9,7 +9,7 @@ namespace Database.Resource.Implementation
         [MemoryOffset(44)] [XdbElement] public Aabb Aabb;
         [MemoryOffset(40)] [XdbElement] public Bool AreaPart;
         [MemoryOffset(36)] [XdbElement] public Int EndElement;
-        [MemoryArrayOffset(20, 12)] [XdbArray] public AsciiString[] ImplicitAreas; //TODO: Array of AsciiString
+        [MemoryArrayOffset(20, 12)] [XdbArray] public AsciiString[] ImplicitAreas;
         [MemoryOffset(8)] [XdbElement] public AsciiString Name;
         [MemoryOffset(32)] [XdbElement] public Int StartElement;
     }
