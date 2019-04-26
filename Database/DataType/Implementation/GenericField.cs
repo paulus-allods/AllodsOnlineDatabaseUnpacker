9eddc779f71d89c3630b093a5bced1fe5a153bea
Paulus-AllodsLegacy <paulus.allods.nova@gmail.com>
2019-04-25 22:52:03 -0700

    Add generic field

---
+++ Database/DataType/Implementation/GenericField.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using Database.Resource.Enum;
+
+namespace Database.DataType.Implementation
+{
+    public class GenericField<T> : DataType where T : Resource.Resource
+    {
+        private T _content;
+        private string _className;
+        
+        public override void Deserialize(IntPtr memoryAddress)
+        {
+            var objectAddress = Marshal.ReadIntPtr(memoryAddress);
+            if (objectAddress != IntPtr.Zero)
+            {
+                var metaDataAddress = Marshal.ReadIntPtr(objectAddress + 12);
+                var className = new AsciiString();
+                className.Deserialize(metaDataAddress + 16);
+                _className = className.ToString().Split('.').Last();
+                var type = Type.GetType($"Database.Resource.Implementation.{_className}");
+                if (type is null)
+                {
+                    throw new Exception("Type is null");
+                }
+
+                if (!typeof(T).IsAssignableFrom(type))
+                {
+                    throw new Exception($"Cannot assign {typeof(T)} from {type}");
+                }
+                _content = Activator.CreateInstance(type) as T;
+                if (_content is null)
+                {
+                    throw new Exception("Content is null");
+                }
+                _content.Deserialize(objectAddress);
+            }
+        }
+
+        public override XElement Serialize(string name)
+        {
+            if (_content is null)
+            {
+                return new XElement(name);
+            }
+            var content = _content.Serialize(name);
+            content.SetAttributeValue("type",_className);
+            return content;
+        }
+    }
+}
