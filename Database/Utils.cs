72c3e8ec2531457a2d50132b36f2eb33bc01af7f
Paulus-AllodsLegacy <paulus.allods.nova@gmail.com>
2019-06-25 23:44:46 -0700

    Add utils functions

---
+++ Database/Utils.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace Database
+{
+    public static class Utils
+    {
+        private static readonly Regex ClassRegex = new Regex(@"\((.+)\)\.xdb");
+        private static readonly Regex ClassRegex2 = new Regex(@"(.+)\.xdb");
+
+        enum Directory
+        {
+            Characters,
+            Client,
+            Creatures,
+            Editor,
+            Interface,
+            ItemMall,
+            Items,
+            Maps,
+            Material,
+            Mechanics,
+            Mods,
+            SFX,
+            Ships,
+            Spells,
+            System,
+            World
+        }
+        
+        
+        public static string GetClassName(string filePath)
+        {
+            var match = ClassRegex.Match(filePath);
+            if (!match.Success)
+            {
+                match = ClassRegex2.Match(filePath);
+                if (!match.Success)
+                {
+                    throw new Exception($"Cannot find class name for {filePath}");
+                }
+            }
+            return match.Groups[1].ToString();
+            
+        }
+
+        public static string NormalizePath(string filePath)
+        {
+            var parts = filePath.Split('/');
+            if (parts.Length > 1 && Enum.IsDefined(typeof(Directory), parts[0]))
+            {
+                return $"/{filePath}";
+            }
+            return filePath;
+        }
+    }
+}
