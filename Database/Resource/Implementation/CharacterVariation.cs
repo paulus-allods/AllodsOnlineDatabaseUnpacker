1e4a8979b431f05c7fe792c2cf66bec75db18940
Paulus-AllodsLegacy <paulus.allods.nova@gmail.com>
2019-07-23 01:57:47 -0700

    Fix CharacterVariation

--- Database/Resource/Implementation/CharacterVariation.cs
+++ Database/Resource/Implementation/CharacterVariation.cs
@@ -6,15 +6,14 @@ namespace Database.Resource.Implementation
 {
     public class CharacterVariation : Resource
     {
-        [MemoryOffset(64)] [XdbElement] public FileRef AdditionalVariation;
-        [MemoryOffset(56)] [XdbElement] public FileRef Face;
+        [MemoryOffset(68)] [XdbElement] public FileRef AdditionalVariation;
+        [MemoryOffset(60)] [XdbElement] public FileRef Face;
         [MemoryOffset(52)] [XdbElement] public FileRef Facial;
         [MemoryOffset(48)] [XdbElement] public Int FacialColor;
         [MemoryOffset(44)] [XdbElement] public Int HairColor;
         [MemoryOffset(36)] [XdbElement] public FileRef HairGeoset;
         [MemoryOffset(4)] [XdbElement] public FileRef Skin;
         [MemoryOffset(0)] [XdbElement] public Int SkinColor;
-
-        //TODO: [MemoryOffset(0)] [XdbElement]public ModelMorphPreset ModelMorphPreset;
+        [MemoryOffset(12)] [XdbElement] public ModelMorphPreset MorphPreset;
     }
 }
