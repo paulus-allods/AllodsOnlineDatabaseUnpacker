9ed22c2aaa4243a2674e32e1f6ae1dc6fca7ad18
Paulus-AllodsLegacy <paulus.allods.nova@gmail.com>
2019-07-30 01:40:16 -0700

    Code cleanup

--- Database/Resource/Implementation/CollisionMesh.cs
+++ Database/Resource/Implementation/CollisionMesh.cs
@@ -9,9 +9,9 @@ namespace Database.Resource.Implementation
         [MemoryOffset(72)] [XdbElement] public Aabb Aabb;
         [MemoryOffset(60)] [XdbElement] public TextFileRef BinaryFile;
         [MemoryOffset(56)] [XdbElement] public Int BinaryVersion;
-        [MemoryOffset(28)] [XdbElement] public FileRef DefaultMaterial;
-        [MemoryArrayOffset(36, 8)] [XdbArray] public FileRef[] CustomMaterials;
         [MemoryOffset(52)] [XdbElement] public Int ClipMask;
+        [MemoryArrayOffset(36, 8)] [XdbArray] public FileRef[] CustomMaterials;
+        [MemoryOffset(28)] [XdbElement] public FileRef DefaultMaterial;
         [MemoryOffset(24)] [XdbElement("SourceFileCRC")] public Int SourceFileCrc;
     }
 }
